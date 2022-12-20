d83ac6d2ba656176169b1f00825b8f1b57b8ad70
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-20 19:47:12 +0200

    Add basic express middleware

--- server.js
+++ server.js
@@ -4,9 +4,30 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/users', (req, res) => {
-  res.send([{ id: 1 }, { id: 2 }]);
-});
+// app.get('/users', (req, res) => {
+//   res.send([{ id: 1 }, { id: 2 }]);
+// });
+
+app.post(
+  '/users',
+  (req, res, next) => {
+    console.log('first middleware');
+
+    if(true) {
+      next();
+    } else {
+      res.send('bad stuff happened');
+    }
+  },
+  (req, res, next) => {
+    console.log('second middleware');
+    next();
+  },
+  (req, res) => {
+    console.log('final func');
+    res.send('user created');
+  }
+);
 
 app.get('/test*', (req, res) => {
   res.send(`request.path is ${req.path} and request.method is ${req.method}`);
