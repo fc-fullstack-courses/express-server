943b4b65d31bc90b48ccb8c2d1e422600f9e6cba
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-21 19:08:18 +0200

    Add get single user route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,10 @@
-const usersDB = [];
+const usersDB = [
+  {
+    id: 1,
+    login: 'admin',
+    password: 'admin',
+  },
+];
 
 module.exports.createUser = async (req, res) => {
   const newUser = {
@@ -13,4 +19,19 @@ module.exports.createUser = async (req, res) => {
 
 module.exports.getUsers = async (req, res) => {
   res.send(usersDB);
+};
+
+module.exports.getUser = async (req, res) => {
+  const {
+    params: { userId },
+    // query: {page}
+  } = req;
+
+  const foundUser = usersDB.find((u) => u.id === Number(userId));
+
+  if (foundUser) {
+    res.send(foundUser);
+  } else {
+    res.status(404).send('User not found');
+  }
 };
--- server.js
+++ server.js
@@ -6,15 +6,11 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/users', UserController.getUsers);
-
 const bodyParser = express.json();
 
+app.get('/users', UserController.getUsers);
+app.get('/users/:userId', UserController.getUser);
 app.post('/users', bodyParser, validateUserMW, UserController.createUser);
-
-app.get('/test*', (req, res) => {
-  res.send(`request.path is ${req.path} and request.method is ${req.method}`);
-});
 
 /*
 app.post
