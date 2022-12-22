8f905c661b64144627b7e0d22d42bc4a4eaace08
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-22 19:21:21 +0200

    Refactor userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,12 +8,12 @@ const UserController = require('../controllers/user.controller');
 const userRouter = express.Router();
 
 userRouter
-  .route('/users')
+  .route('/')
   .get(UserController.getUsers)
   .post(validateUserMW, UserController.createUser);
 
 userRouter
-  .route('/users/:userId')
+  .route('/:userId')
   .get(UserController.getUser)
   .put(validateUserUpdateMW, UserController.updateUser)
   .delete(UserController.deleteUser);
