11c9c9d3ee686977e3f8c2638e3a24092a2adf83
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-22 18:38:44 +0200

    Remove comments

--- app.js
+++ app.js
@@ -14,23 +14,10 @@ app
   .get(UserController.getUsers)
   .post(validateUserMW, UserController.createUser);
 
-// app.get('/users', UserController.getUsers);
-// app.post('/users', validateUserMW, UserController.createUser);
-
 app
   .route('/users/:userId')
   .get(UserController.getUser)
   .put(validateUserUpdateMW, UserController.updateUser)
   .delete(UserController.deleteUser);
-
-// app.get('/users/:userId', UserController.getUser);
-// app.delete('/users/:userId', UserController.deleteUser);
-// app.put('/users/:userId', validateUserUpdateMW, UserController.updateUser);
-/*
-app.post
-app.put
-app.patch
-app.delete
-*/
 
 module.exports = app;
